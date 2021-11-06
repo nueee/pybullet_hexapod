2a8635bc6d3eb07041192e93fe38bf304fd280d3
nueee <nue16@snu.ac.kr>
2021-11-06 19:14:16 +0900

    first phantomax simulation! we need to fix rendering, add some advanced env ftns, etc.

--- hexapod/resources/hexapod.py
+++ hexapod/resources/hexapod.py
@@ -1,82 +1,60 @@
 import pybullet as p
 import os
 import math
+import numpy as np
 
 
-class Car:
+class Hexapod:
     def __init__(self, client):
         self.client = client
-        f_name = os.path.join(os.path.dirname(__file__), 'simplecar.urdf')
-        self.car = p.loadURDF(fileName=f_name,
-                              basePosition=[0, 0, 0.1],
-                              physicsClientId=client)
+        f_name = os.path.join(
+            os.path.dirname(__file__),
+            'ASSY_phantom_urdf/urdf/ASSY_phantom_urdf.urdf'
+        )
+        self.hexapod = p.loadURDF(
+            fileName=f_name,
+            basePosition=[0, 0, 0.1],
+            baseOrientation=p.getQuaternionFromEuler([np.pi/2, 0, 0]),
+            physicsClientId=client
+        )
 
-        # Joint indices as found by p.getJointInfo()
-        self.steering_joints = [0, 2]
-        self.drive_joints = [1, 3, 4, 5]
-        # Joint speed
-        self.joint_speed = 0
-        # Drag constants
-        self.c_rolling = 0.2
-        self.c_drag = 0.01
-        # Throttle constant increases "speed" of the car
-        self.c_throttle = 20
+        self.legJoints = range(1, 19)
+        self.jointForces = np.array([1.0]*18)
 
     def get_ids(self):
-        return self.car, self.client
+        return self.hexapod, self.client
 
     def apply_action(self, action):
-        # Expects action to be two dimensional
-        throttle, steering_angle = action
-
-        # Clip throttle and steering angle to reasonable values
-        throttle = min(max(throttle, 0), 1)
-        steering_angle = max(min(steering_angle, 0.6), -0.6)
-
-        # Set the steering joint positions
-        p.setJointMotorControlArray(self.car, self.steering_joints,
-                                    controlMode=p.POSITION_CONTROL,
-                                    targetPositions=[steering_angle] * 2,
-                                    physicsClientId=self.client)
-
-        # Calculate drag / mechanical resistance ourselves
-        # Using velocity control, as torque control requires precise models
-        friction = -self.joint_speed * (self.joint_speed * self.c_drag +
-                                        self.c_rolling)
-        acceleration = self.c_throttle * throttle + friction
-        # Each time step is 1/240 of a second
-        self.joint_speed = self.joint_speed + 1/30 * acceleration
-        if self.joint_speed < 0:
-            self.joint_speed = 0
-
-        # Set the velocity of the wheel joints directly
+        p.setJointMotorControl2(
+            self.hexapod,
+            0,
+            controlMode=p.POSITION_CONTROL,
+            force=0,
+            physicsClientId=self.client
+        )
         p.setJointMotorControlArray(
-            bodyUniqueId=self.car,
-            jointIndices=self.drive_joints,
-            controlMode=p.VELOCITY_CONTROL,
-            targetVelocities=[self.joint_speed] * 4,
-            forces=[1.2] * 4,
-            physicsClientId=self.client)
+            self.hexapod,
+            self.legJoints,
+            controlMode=p.POSITION_CONTROL,
+            targetPositions=action,
+            forces=self.jointForces,
+            physicsClientId=self.client
+        )
 
     def get_observation(self):
-        # Get the position and orientation of the car in the simulation
-        pos, ang = p.getBasePositionAndOrientation(self.car, self.client)
-        ang = p.getEulerFromQuaternion(ang)
-        ori = (math.cos(ang[2]), math.sin(ang[2]))
-        pos = pos[:2]
-        # Get the velocity of the car
-        vel = p.getBaseVelocity(self.car, self.client)[0][0:2]
-
-        # Concatenate position, orientation, velocity
-        observation = (pos + ori + vel)
+        # Get the joint position
+        observation = np.array(p.getJointStates(self.hexapod, self.legJoints, self.client))[:, 0]
 
         return observation
 
+    def get_position(self):
+        pos, ang = p.getBasePositionAndOrientation(self.hexapod, self.client)
+        ang = p.getEulerFromQuaternion(ang)
 
+        return pos, ang
 
-
-
-
-
-
-
+    # def reset_hexapod(self):
+    #     for i in range(p.getNumJoints(self.hexapod, self.client)):
+    #         p.resetJointState(self.hexapod, i, 0.0, 0.0, self.client)
+    #     p.resetBaseVelocity(self.hexapod, [0.0]*3, [0.0]*3, self.client)
+    #     p.resetBasePositionAndOrientation(self.hexapod, [0.0, 0.0, 0.1], [0.0]*4, self.client)
--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import gym
-import simple_driving
+import numpy as np
+import hexapod
 import time
 from stable_baselines3 import PPO
 
+
 def main():
-    env = gym.make("SimpleDriving-v0")
+    env = gym.make("Hexapod-v0")
     model = PPO(
         "MlpPolicy",
         env=env,
         verbose=2,
     )
 
-    model.learn(int(1e5))
+    model.learn(int(1e3))
 
-    ob = env.reset()
+    obs = env.reset()
     while True:
-        action, _ = model.predict(ob)
-        ob, _, done, _ = env.step(action)
+        action, _ = model.predict(obs.astype(np.float32))
+        obs, _, done, _ = env.step(action)
         env.render()
         if done:
-            ob = env.reset()
-            time.sleep(1/30)
+            obs = env.reset()
+            time.sleep(1/60)
 
 
 if __name__ == '__main__':
