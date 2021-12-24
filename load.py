2ad8da1e8102dc91386eb4b3c9d3c51975f39d99
nueee <nue16@snu.ac.kr>
2021-12-24 18:07:24 +0900

    impl fixed torso

--- load.py
+++ load.py
@@ -5,21 +5,18 @@ import time
 from stable_baselines3 import PPO
 
 
-def main():
-    rendering = gym.make("HexapodRender-v0")
-    model = PPO.load("./save_model_1127/A/hexapod_model_1127A_1000000_steps.zip")
+rendering = gym.make("HexapodRender-v0")
+model = PPO.load("./save_model_1127/A/hexapod_model_1127A_1000000_steps.zip")
 
-    # start rendering the current model.
-    obs = rendering.reset()
-    for i in range(1000):
-        action, _ = model.predict(obs.astype(np.float32))
-        obs, _, done, _ = rendering.step(action)
-        rendering.render()
-        if done:
-            obs = rendering.reset()
+# start rendering the current model.
+obs = rendering.reset()
+for i in range(1000):
+    action, _ = model.predict(obs.astype(np.float32))
+    obs, _, done, _ = rendering.step(action)
+    rendering.render()
+    if done:
+        obs = rendering.reset()
 
-if __name__ == '__main__':
-    main()
 
 
 
