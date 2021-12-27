632de7ffd8a54c82349485168bee72972a61615f
waitzkin <bes04075@naver.com>
2021-12-27 17:39:16 +0900

    12/27 SA primary implementation added into ST

--- simulation_tuning/simul_err_calc.py
+++ simulation_tuning/simul_err_calc.py
@@ -8,6 +8,8 @@ import hexapod
 import time
 from stable_baselines3 import PPO
 import configparser
+import random
+import math
 
 # overall var
 model_path = "../save_model_1226/power/hexapod_model_1226power_500000_steps.zip"
@@ -42,6 +44,11 @@ for i in range(total_timesteps*sample_rate):
 
 print(commandValues[15][5])
 
+def out(state):
+    if state[0]<bf_lower_limit[0] or state[0]>bf_upper_limit[0] or state[1]<bf_lower_limit[1] or state[1]>bf_upper_limit[1]:
+        return True
+    else:
+        return False
 def write_alpha(a):
     edit = configparser.ConfigParser()
     edit.read("../configfile.ini")
@@ -134,10 +141,86 @@ def get_GD_alpha(a0):
     return nowalpha,E(nowalpha)
 
 
+def simulated_annealing(initial_state):
+    """Peforms simulated annealing to find a solution"""
+    initial_temp = 900
+    final_temp = .1
+    dT = 0.01
+
+    current_temp = initial_temp
+
+    # Start by initializing the current state with the initial state
+    current_state = initial_state
+    solution = current_state
+
+    min_state = initial_state
+
+    while current_temp > final_temp:
+        #print("now on"+str(solution))
+        neighbor = get_neighbors(solution)
+
+        # Check if neighbor is best so far
+        cost_diff = get_cost(current_state) - get_cost(neighbor)
+
+        # if the new solution is better, accept it
+        if cost_diff > 0:
+            solution = neighbor
+            if get_cost(solution)<get_cost(min_state):
+                min_state = solution
+        # if the new solution is not better, accept it with a probability of e^(-cost/temp)
+        else:
+            if random.uniform(0, 1) < math.exp(-cost_diff / current_temp):
+                solution = neighbor
+        # decrement the temperature
+        current_temp -= dT
+
+    return min_state, get_cost(min_state)
+
+
+def get_cost(state):
+    """Calculates cost of the argument state for your solution."""
+    #print("cost of state"+str(state))
+    #print(state[0]**2+state[1]**2)
+    return E(state)
+    return (state[0]-0.2)**2+(state[1]-0.1)**2
+
+
+def get_neighbors(state):
+    """Returns neighbors of the argument state for your solution."""
+    '''
+    p = random.uniform(0,1)
+    if p<0.25:
+        return [state[0]+0.01,state[1]]
+    elif p<0.5:
+        return [state[0] - 0.01, state[1]]
+    elif p<0.75:
+        return [state[0], state[1] + 0.01 ]
+    else:
+        return [state[0], state[1] - 0.01 ]
+    '''
+    ret = random.choice([[state[0]+0.01,state[1]],[state[0] - 0.01, state[1]],[state[0], state[1] + 0.01 ],[state[0], state[1] - 0.01 ]])
+    while out(ret):
+        ret = random.choice([[state[0] + 0.01, state[1]], [state[0] - 0.01, state[1]], [state[0], state[1] + 0.01],
+                             [state[0], state[1] - 0.01]])
+    return ret
+
+
+
+
 write_alpha([0,0])
 
 print(E([0.2,0.1]))
 
+print("init")
+x0 = [0,0]
+print(get_cost(x0))
+ANS = 10000
+for i in range(1):
+    print("in"+str(i))
+    t = simulated_annealing(x0)[1]
+    print(t)
+    ANS = min(t,ANS)
+print(ANS)
 #print(get_brute_force_alpha())
 
 #print(get_GD_alpha())
--- simulation_tuning/simulated_annealing_example.py
+++ simulation_tuning/simulated_annealing_example.py
@@ -2,9 +2,15 @@ import random
 import math
 
 # https://ichi.pro/ko/python-eseo-simyulleisyeon-doen-eonilling-algolijeum-eul-guhyeonhaneun-bangbeob-188125677573792
+
+def out(state):
+    if abs(state[0])>1 or abs(state[1])>1:
+        return True
+    else:
+        return False
 def simulated_annealing(initial_state):
     """Peforms simulated annealing to find a solution"""
-    initial_temp = 9
+    initial_temp = 900
     final_temp = .1
     alpha = 0.01
 
@@ -14,8 +20,10 @@ def simulated_annealing(initial_state):
     current_state = initial_state
     solution = current_state
 
+    min_state = initial_state
+
     while current_temp > final_temp:
-        print("now on"+str(solution))
+        #print("now on"+str(solution))
         neighbor = get_neighbors(solution)
 
         # Check if neighbor is best so far
@@ -24,6 +32,8 @@ def simulated_annealing(initial_state):
         # if the new solution is better, accept it
         if cost_diff > 0:
             solution = neighbor
+            if get_cost(solution)<get_cost(min_state):
+                min_state = solution
         # if the new solution is not better, accept it with a probability of e^(-cost/temp)
         else:
             if random.uniform(0, 1) < math.exp(-cost_diff / current_temp):
@@ -31,18 +41,19 @@ def simulated_annealing(initial_state):
         # decrement the temperature
         current_temp -= alpha
 
-    return solution, get_cost(solution)
+    return min_state, get_cost(min_state)
 
 
 def get_cost(state):
     """Calculates cost of the argument state for your solution."""
-    print("cost of state"+str(state))
-    print(state[0]**2+state[1]**2)
-    return state[0]**2+state[1]**2
+    #print("cost of state"+str(state))
+    #print(state[0]**2+state[1]**2)
+    return state[0]**3+state[1]**3
 
 
 def get_neighbors(state):
     """Returns neighbors of the argument state for your solution."""
+    '''
     p = random.uniform(0,1)
     if p<0.25:
         return [state[0]+0.01,state[1]]
@@ -52,11 +63,23 @@ def get_neighbors(state):
         return [state[0], state[1] + 0.01 ]
     else:
         return [state[0], state[1] - 0.01 ]
+    '''
+    ret = random.choice([[state[0]+0.01,state[1]],[state[0] - 0.01, state[1]],[state[0], state[1] + 0.01 ],[state[0], state[1] - 0.01 ]])
+    while out(ret):
+        ret = random.choice([[state[0] + 0.01, state[1]], [state[0] - 0.01, state[1]], [state[0], state[1] + 0.01],
+                             [state[0], state[1] - 0.01]])
+    return ret
+
+
 
 
 print("init")
-print(get_cost([0.4,0.4]))
+x0 = [1,1]
+print(get_cost(x0))
 ANS = 10000
 for i in range(100):
-    ANS = min(simulated_annealing([0.4,0.4])[1],ANS)
+    print("in"+str(i))
+    t = simulated_annealing(x0)[1]
+    print(t)
+    ANS = min(t,ANS)
 print(ANS)
